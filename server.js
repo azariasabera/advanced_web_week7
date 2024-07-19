e48d6b77989a3d46967d73012ab3393b420219af
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-19 19:31:11 +0300

    Done with task 2

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bcrypt = require('bcryptjs');
+const session = require('express-session')
 
 let users = []
 
 app.set('view-engine', 'pug')   
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(session
+    ({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: false
+    }))
 
 app.get('/', (req, res) => {
     res.render('index.pug')
@@ -38,6 +45,29 @@ app.get('/api/user/list', (req, res)=>{
     res.json(users)
 })
 
+app.get('/api/user/login', (req, res)=>{
+    res.render('login.pug')
+});
+
+app.post('/api/user/login', async (req, res)=>{
+    let checkUser = users.find(user => user.username === req.body.username)
+    if (checkUser) {
+        try {
+            if (await bcrypt.compare(req.body.password, checkUser.password)) {
+                req.session.user = checkUser
+                res.send('Login Successful')
+            } else {
+                res.status(401).send('Login Failed')
+            }
+        } catch (error) {
+            res.status(500).send(`Error occured: ${error}`)
+        }
+    } else {
+        res.status(401).send('User not found')
+    }
+});
+
+    
 app.listen(port, ()=>{
     console.log(`Listening to port ${port}`)
 })
