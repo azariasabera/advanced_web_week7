8916486bb33d45486b5a98013cd4b759338e2dd7
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-19 17:49:09 +0300

    Made modifications to task 1

--- server.js
+++ server.js
@@ -15,19 +15,27 @@ app.get('/', (req, res) => {
 
 app.post('/api/user/register', async (req, res)=>{
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        let newUser = {
-            id: Date.now().toString(),
-            username: req.body.username,
-            password: hashedPassword
+        let userCheck = users.find(user => user.username === req.body.username)
+        if (!userCheck) { 
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            const newUser = {
+                id: Date.now().toString(), // this is just a temporary solution to generate a unique id
+                username: req.body.username,
+                password: hashedPassword
+            }
+            users.push(newUser);
+            res.json(newUser)
+        } else {
+            res.send("User already exists!")
         }
-        users.push(newUser);
-        console.log(users)
-        res.json(newUser)
 
     } catch (error) {
         res.status(400).send(`Error occured: ${error}`)
     }
+})
+
+app.get('/api/user/list', (req, res)=>{
+    res.json(users)
 })
 
 app.listen(port, ()=>{
